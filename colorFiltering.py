0ffea25792c4ac3f064817320b301aaa5f1e2529
Lucas Bonde <lucas.nesgaard.bonde@gmail.com>
2022-11-18 08:37:43 +0100

    Tried making the RGB filtering a Function()

--- colorFiltering.py
+++ colorFiltering.py
@@ -4,14 +4,13 @@ import numpy as np
 # Make ROI
 img = cv2.imread('RGB_color_pic_Color.png')
 
+
+
 roi = img[0:720, 120:600] #[y-start : y-stop, x-start: x-stop]
-# Add brightness/contrast
-
-# Sort away background based on Distance
-
 # Color Thresholding for Trunk
 hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
+#Colorthreshold might be changed
 minThresh = np.array([20,28,30])
 maxThresh = np.array([114,100, 115])
 
@@ -22,7 +21,13 @@ kernel = np.ones((5,5),np.uint8)
 opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-color = cv2.bitwise_and(roi,roi, mask=opening) #Color res, after opening/closing
+closing = cv2.bitwise_and(roi, roi, mask=opening) #Color res, after opening/closing
+
+
+
+cv2.imshow('output', closing)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # Make Binary
 
@@ -39,11 +44,3 @@ color = cv2.bitwise_and(roi,roi, mask=opening) #Color res, after opening/closing
 
 #cv2.imshow('Grayscaled Img', grayscaled)
 #cv2.imshow('Binary Img', threshold)
-cv2.imshow('original', img)
-cv2.imshow('mask', mask)
-cv2.imshow('res', res)
-cv2.imshow('opening', opening)
-cv2.imshow('closing', closing)
-cv2.imshow('color', color)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
