88ab34d2e6e1003acc76dc6752f5f9ace17fde9d
Lucas Bonde <lucas.nesgaard.bonde@gmail.com>
2022-11-21 15:28:47 +0100

    Update colorFiltering.py

--- colorFiltering.py
+++ colorFiltering.py
@@ -42,9 +42,3 @@ def main():
 
 
 
-#grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#retval, threshold = cv2.threshold(grayscaled, 50, 255, cv2.THRESH_BINARY)
-#gaus = cv2.adaptiveThreshold(grayscaled, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
-
-#cv2.imshow('Grayscaled Img', grayscaled)
-#cv2.imshow('Binary Img', threshold)
